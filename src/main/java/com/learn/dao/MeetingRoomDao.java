4dde51813560cad1dd1a4d0395685bd804efad45
Godwao <353034524@qq.com>
2017-05-25 23:57:24 +0800

    会议室管理（不包含会议管理）

---
+++ src/main/java/com/learn/dao/MeetingRoomDao.java
@@ -0,0 +1,19 @@
+package com.learn.dao;
+
+import com.learn.model.MeetingRoom;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * Created by Wangqingyu on 2017/5/25.
+ */
+@Mapper
+public interface MeetingRoomDao {
+    void insertMeetingRoom(MeetingRoom meetingRoom);
+    List<MeetingRoom> selectMeetingRoom(@Param("offset") int offset,@Param("length")int length,@Param("meetingRoomName")String meetingRoomName,@Param("meetingRoomNumber")int meetingRoomNumber);
+    int getTotal(@Param("offset") int offset,@Param("length")int length,@Param("meetingRoomName")String meetingRoomName,@Param("meetingRoomNumber")int meetingRoomNumber);
+    void updateMeetingRoom(MeetingRoom meetingRoom);
+
+}
